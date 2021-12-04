83a8e2261d001bb47ac8a12e63b9cfb776fd2dfa
Aidan <aidancampbell738@gmail.com>
2021-12-04 16:14:03 -0500

    Working on printer

--- printer.cc
+++ printer.cc
@@ -3,7 +3,77 @@
 
 using namespace std;
 
-Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+Printer::Printer(unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers) : 
+    numStudents(numStudents), numVendingMachines(numVendingMachines), numCouriers(numCouriers) {
+    numCols = 6 + numStudents + numVendingMachines + numCouriers;
+    buffer = new Data[numCols];//create buffer array
+
+    //Print all titles
+    cout << "Parent\tGropoff\tWATOff\tNames\tTruck\tPlant";
+    for(int i = 0; i < numStudents; i++) {//students
+        cout << "\tStud" << i;
+    }
+    for(int i = 0; i < numVendingMachines; i++) {//vending machines
+        cout << "\tMach" << i;
+    }
+    for(int i = 0; i < numCouriers; i++) {//courriers
+        cout << "\tCour" << i;
+    }
+    cout << endl;
+
+    //Print *******'s
+    for(int i = 0; i < voters; i++) {
+        if(i > 0) cout << "\t";
+        cout << "*******";
+    }
+    cout << endl;
+}
+
+//Non-trivial destructor
+//delete the buffer, print last values, and print final message
+Printer::~Printer() {
+    for(int i = 0; i < voters; i++) {
+        if(buffer[i].assigned) {//print any messages that haven't been printed yet
+            flush();
+            break;
+        }
+    }
+    delete[] buffer;//clean up
+    cout << "*****************" << endl;//final message
+}
+
+//This method will print a line of text
+//this method is called after print attempts to write to a buffer index
+//whose "assigned" boolean is already true
+void Printer::flush() {
+    int blankSpaces = -1;//used to configure whitespace (blankSpace represents the number of blank columns in the line)
+    for(int i = 0; i < numCols; i++) {
+        if(buffer[i].assigned) {//only print stuff for voters with data to print
+            for(int j = 0; j < blankSpaces + 1 && i > 0; j++) {
+                cout << "\t";//print tab characters
+            }
+            blankSpaces = 0;
+            switch(buffer[i].state) {//print data based on state
+                case Voter::Start: cout << "S"; break;
+                case Voter::Vote: cout << "V " << buffer[i].vote.picture << "," << buffer[i].vote.statue << "," << buffer[i].vote.giftshop; break;
+                case Voter::Block: cout << "B " << buffer[i].numBlocked; break;
+                case Voter::Unblock: cout << "U " << buffer[i].numBlocked; break;
+                case Voter::Barging: cout << "b " << buffer[i].numBlocked << " " << buffer[i].group; break;
+                case Voter::Done: cout << "D"; break;
+                case Voter::Complete: cout << "C " << buffer[i].tour.tourkind; break;
+                case Voter::Going: cout << "G " << buffer[i].tour.tourkind << " " << buffer[i].tour.groupno; break;
+                case Voter::Failed: cout << "X"; break;
+                case Voter::Terminated: cout << "T"; remainingVoters--; break;
+            }
+        } else {
+            blankSpaces++;
+        }
+        buffer[i].assigned = false;//allow each voter's print data to be overwritten for next flush
+    }
+    cout << endl;
+}
+
+
 void print( Kind kind, char state );
 void print( Kind kind, char state, unsigned int value1 );
 void print( Kind kind, char state, unsigned int value1, unsigned int value2 );
--- printer.h
+++ printer.h
@@ -1,7 +1,26 @@
+#ifndef _PRINTER_
+#define _PRINTER_
+
 _Monitor Printer {
+    int numStudents, numVendingMachines, numCouriers, numCols;
+    //Struct to store data used for printing
+    //alternative to building and storing strings
+    struct Data {
+        bool assigned = false;//used to determine when to flush
+        char state;//state (S, D, N, etc.)
+        //the variables below are ints passed in print members
+        //these dataValues take on the form of whatever data accompanies the print message
+        //For example, for a parent's D message, dataValue1 would represent the student s
+        //receiving the gift and dataValue2 would represent the amount of gift g
+        int dataValue1, int dataValue2;
+    };
+    Data* buffer;//array to store each Kind's next print data
+    void flush();//print a line of data
+
   public:
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+    ~Printer();//added destructor
 	void print( Kind kind, char state );
 	void print( Kind kind, char state, unsigned int value1 );
 	void print( Kind kind, char state, unsigned int value1, unsigned int value2 );
@@ -9,3 +28,5 @@ _Monitor Printer {
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1 );
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2 );
 };
+
+#endif
