7feef6f8867f98863f6496093129a06ef2346ee6
Aidan <aidancampbell738@gmail.com>
2021-12-04 18:20:55 -0500

    Added watcard

---
+++ watcard.cc
@@ -0,0 +1,16 @@
+#include "watcard.h"
+
+//deposit amount into watcard -> increase balance by amount
+void WATCard::deposit(unsigned int amount) {
+    balance += amount;
+}
+
+//withdraw amount from watcard balance -> decrease balance by amount
+void WATCard::withdraw(unsigned int amount) {
+    balance -= amount;
+}
+
+//getter function for balance
+unsigned int getBalance() {
+    return balance;
+}
---
+++ watcard.h
@@ -0,0 +1,18 @@
+#ifndef _WATCARD_
+#define _WATCARD_
+
+class WATCard {
+	WATCard(const WATCard &) = delete;	// prevent copying
+	WATCard& operator=(const WATCard &) = delete;
+
+    unsigned int balance = 0;//stores current watcard balance
+
+  public:
+	typedef Future_ISM<WATCard *> FWATCard;	// future watcard pointer
+	WATCard() {}
+	void deposit(unsigned int amount);
+	void withdraw(unsigned int amount);
+	unsigned int getBalance();
+};
+
+#endif
