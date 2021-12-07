9abbb3cd9aa293645c3b2e49d64ef658c31ee83d
Aidan <aidancampbell738@gmail.com>
2021-12-06 23:17:49 -0500

    Student done

--- student.cc
+++ student.cc
@@ -1,7 +1,69 @@
 #include "student.h"
+#include "printer.h"
+#include "nameserver.h"
+#include "watcardoffice.h"
+#include "groupoff.h"
+#include "vendingmachine.h"
 
 using namespace std;
 
-void Student::main() {
+//Helper function to prevent code duplication
+//Will get a vending machine from nameserver and print the appropriate message
+VendingMachine* Student::getVM() {
+    VendingMachine* vm = nameServer.getMachine(id);//get machine
+    printer.print(Printer::student, id, 'V', vm->getId());//print message
+    return vm;
+}
 
+//Student Task main
+//It will first initialize the number of soda purchases, the favourite flavour, the watcard, giftcard, and initial vending machine
+//After, it will loop to purchase numPurchases favourite soda
+//A purchase attempt consists of a second for loop that will iterate until a buy is successful
+//An unsuccessful buy attempt will be handled by the relevant catch block (lost, free, funds or stock)
+//and then another attempt will be made (without exiting the inner for loop)
+void Student::main() {
+    int numPurchases = mprng(1, maxPurchases);//number of sodas to buy
+    int favFlavour = mprng(3);//favourite flavour
+    printer.print(Printer::student, id, 'S', favFlavour, numPurchases);
+
+    WATCard::FWATCard myWatcard = cardOffice.create(id, 5);//create watcard
+    WATCard::FWATCard myGiftcard = groupoff.giftCard();//get giftcard
+    VendingMachine* vendingmachine = getVM();//get initial vending machine
+
+    //Buy numPurchases number of favourite soda
+    for(int i = 0; i < numPurchases; i++) {
+        yield(mprng(1, 10));//randomly yield before attempting a purchase
+
+        //Keep looping until a purchase has been made
+        for(;;) {
+            try {
+                //Choose a giftcard, if available
+                if(myGiftcard.available()) {
+                    vendingmachine.buy(favFlavour, myGiftcard);//try to buy
+                    printer.print(Printer::student, id, 'G', favFlavour, myGiftcard.getBalance());
+                } else {
+                    vendingmachine.buy(favFlavour, myWatcard);//try to buy
+                    printer.print(Printer::student, id, 'B', favFlavour, myWatcard.getBalance());
+                }
+                break;//exit for loop on successful purchase
+            } catch(WATCardOffice::Lost) {//Watcard was lost by courier
+                printer.print(Printer::student, id, 'L');//print message
+                myWatcard = cardOffice.create(id, 5);//get new watcard
+            } catch(VendingMachine::Free) {//free soda
+                if(myGiftcard.available()) {//print message
+                    printer.print(Printer::student, id, 'a', favFlavour, myGiftcard.getBalance());
+                } else {
+                    printer.print(Printer::student, id, 'A', favFlavour, myWatcard.getBalance());
+                }
+                yield(4);//drink free soda
+            } catch(VendingMachine::Funds) {//not enough money to buy soda
+                myWatcard = cardOffice.transfer(id, vendingmachine.cost() + 5, &myWatcard);//get more money
+                yield(mprng(1, 10));//yield before trying again
+            } catch(VendingMachine::Stock) {//favourite soda out of stock
+                vendingmachine = getVM();//try a different vending machine
+                yield(mprng(1, 10));//yield before trying again
+            }
+        }
+    }
+    printer.print(Printer::student, id, 'F');
 }
--- student.h
+++ student.h
@@ -1,11 +1,27 @@
 #ifdef _STUDENT_
 #define _STUDENT_
+#include "printer.h"
+#include "nameserver.h"
+#include "watcardoffice.h"
+#include "groupoff.h"
+
+//use global random variable declared in a6main.cc
+extern MPRNG mprng;
 
 _Task Student {
-	void main();
+    Printer &printer;//Printer reference
+    NameServer& nameServer;//NameServer reference
+    WATCardOffice& cardOffice;//WATCardOffice reference
+    Groupoff& groupoff;//Groupoff reference
+    unsigned int id, maxPurchases;//Student id, maximum number of soda purchases
+
+    VendingMachine* getVM();//helper function to prevent code duplication    
+	void main();//tast main
+
   public:
-	Student( Printer & prt, NameServer & nameServer, WATCardOffice & cardOffice, Groupoff & groupoff,
-			 unsigned int id, unsigned int maxPurchases );
+	Student(Printer& prt, NameServer& nameServer, WATCardOffice& cardOffice, Groupoff& groupoff,
+			 unsigned int id, unsigned int maxPurchases) : printer(prt), nameServer(nameServer), 
+             cardOffice(cardOffice), groupoff(groupoff), id(id), maxPurchases(maxPurchases) {}//constructor
 };
 
 #endif
