2111df0ed0d2d095938555e99c4f4dedaabab898
Aidan <aidancampbell738@gmail.com>
2021-12-06 21:06:56 -0500

    Starting Student

---
+++ student.h
@@ -0,0 +1,11 @@
+#ifdef _STUDENT_
+#define _STUDENT_
+
+_Task Student {
+	void main();
+  public:
+	Student( Printer & prt, NameServer & nameServer, WATCardOffice & cardOffice, Groupoff & groupoff,
+			 unsigned int id, unsigned int maxPurchases );
+};
+
+#endif
